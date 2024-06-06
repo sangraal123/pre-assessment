f95214571d6ece431b508ec3f66c3d52c4e0e3ed
sangraal123 <googolheroicquests@gmail.com>
2024-06-07 07:14:14 +0900

    console.logのテストを除去

--- 1/1.js
+++ 1/1.js
@@ -26,7 +26,6 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
 function assessment1() {
     // 上部ヘッダー行を作成
     var header = __spreadArray(["X"], __read(Array.from(Array(12), function (_, i) { return (i + 1).toString(); })), false);
-    console.log(header.slice(1).map(function (i) { return i.toString().padStart(2, ' '); }));
     console.log("".concat(header[0].padStart(2, ' '), " ").concat(header.slice(1).map(function (i) { return i.toString().padStart(3, ' '); }).join(' ')));
     // かけ算表の各行を作成
     for (var row = 1; row <= 12; row++) {
--- 1/1.ts
+++ 1/1.ts
@@ -1,7 +1,6 @@
 function assessment1(): void {
     // 上部ヘッダー行を作成
     const header = ["X", ...Array.from(Array(12), (_, i) => (i + 1).toString())];
-    console.log(header.slice(1).map(i => i.toString().padStart(2, ' ')));
     console.log(`${header[0].padStart(2, ' ')} ${header.slice(1).map(i => i.toString().padStart(3, ' ')).join(' ')}`);
 
     // かけ算表の各行を作成
