9e9162f34efbb0cf2c9ea66c2912373b5e6967da
sangraal123 <googolheroicquests@gmail.com>
2024-06-07 08:57:30 +0900

    微変更

--- 1/1.ts
+++ 1/1.ts
@@ -1,7 +1,7 @@
 function assessment1(): void {
     // 上部ヘッダー行を作成
     const header = ["X", ...Array.from({length: 12}, (_, i) => (i + 1).toString())];
-    console.log(`${header[0].padStart(2, ' ')} ${header.slice(1).map(i => i.toString().padStart(3, ' ')).join(' ')}`);
+    console.log(`${header[0].padStart(2, ' ')} ${header.slice(1).map(i => i.padStart(3, ' ')).join(' ')}`);
 
     // かけ算表の各行を作成
     for (let row = 1; row <= 12; row++) {
