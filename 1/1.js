d3a014cc8626b491c8168beeefa136d7d80961f7
sangraal123 <googolheroicquests@gmail.com>
2024-06-07 07:12:11 +0900

    課題１のアップデート

---
+++ 1/1.js
@@ -0,0 +1,43 @@
+var __read = (this && this.__read) || function (o, n) {
+    var m = typeof Symbol === "function" && o[Symbol.iterator];
+    if (!m) return o;
+    var i = m.call(o), r, ar = [], e;
+    try {
+        while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
+    }
+    catch (error) { e = { error: error }; }
+    finally {
+        try {
+            if (r && !r.done && (m = i["return"])) m.call(i);
+        }
+        finally { if (e) throw e.error; }
+    }
+    return ar;
+};
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+function assessment1() {
+    // 上部ヘッダー行を作成
+    var header = __spreadArray(["X"], __read(Array.from(Array(12), function (_, i) { return (i + 1).toString(); })), false);
+    console.log(header.slice(1).map(function (i) { return i.toString().padStart(2, ' '); }));
+    console.log("".concat(header[0].padStart(2, ' '), " ").concat(header.slice(1).map(function (i) { return i.toString().padStart(3, ' '); }).join(' ')));
+    // かけ算表の各行を作成
+    for (var row = 1; row <= 12; row++) {
+        // 行ヘッダーを開始
+        var rowString = row.toString().padStart(2, ' ');
+        for (var col = 1; col <= 12; col++) {
+            // 各列を適切なスペースで表示
+            rowString += " ".concat(String(row * col).padStart(3, ' '));
+        }
+        console.log(rowString);
+    }
+}
+// 関数を呼び出してかけ算表を表示
+assessment1();
---
+++ 1/1.ts
@@ -0,0 +1,20 @@
+function assessment1(): void {
+    // 上部ヘッダー行を作成
+    const header = ["X", ...Array.from(Array(12), (_, i) => (i + 1).toString())];
+    console.log(header.slice(1).map(i => i.toString().padStart(2, ' ')));
+    console.log(`${header[0].padStart(2, ' ')} ${header.slice(1).map(i => i.toString().padStart(3, ' ')).join(' ')}`);
+
+    // かけ算表の各行を作成
+    for (let row = 1; row <= 12; row++) {
+        // 行ヘッダーを開始
+        let rowString = row.toString().padStart(2, ' ');
+        for (let col = 1; col <= 12; col++) {
+            // 各列を適切なスペースで表示
+            rowString += ` ${String(row * col).padStart(3, ' ')}`;
+        }
+        console.log(rowString);
+    }
+}
+
+// 関数を呼び出してかけ算表を表示
+assessment1();
