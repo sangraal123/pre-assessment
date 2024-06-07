7745d51dfdac0a4aed1236a662d3d6111fa30d32
sangraal123 <googolheroicquests@gmail.com>
2024-06-07 22:18:21 +0900

    型の厳格化

--- 1/1.ts
+++ 1/1.ts
@@ -1,13 +1,13 @@
 function assessment1(): void {
     // 上部ヘッダー行を作成
-    const header = ["X", ...Array.from({length: 12}, (_, i) => (i + 1).toString())];
+    const header: string[] = ["X", ...Array.from({ length: 12 }, (_, i) => (i + 1).toString())];
     console.log(`${header[0].padStart(2, ' ')} ${header.slice(1).map(i => i.padStart(3, ' ')).join(' ')}`);
 
     // かけ算表の各行を作成
-    for (let row = 1; row <= 12; row++) {
+    for (let row: number = 1; row <= 12; row++) {
         // 行ヘッダーを開始
-        let rowString = row.toString().padStart(2, ' ');
-        for (let col = 1; col <= 12; col++) {
+        let rowString: string = row.toString().padStart(2, ' ');
+        for (let col: number = 1; col <= 12; col++) {
             // 各列を適切なスペースで表示
             rowString += ` ${String(row * col).padStart(3, ' ')}`;
         }
